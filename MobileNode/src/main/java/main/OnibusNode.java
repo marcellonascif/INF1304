75b91807cb24b3811ff3d0cac3d80dd736604398
Thomas de Mello Oliva <105178736+Thom3002@users.noreply.github.com>
2024-11-07 13:48:13 +0000

    corrige OnibusNode

--- MobileNode/src/main/java/main/OnibusNode.java
+++ MobileNode/src/main/java/main/OnibusNode.java
@@ -7,11 +7,9 @@ import java.util.UUID;
 public class OnibusNode extends MainCKMobileNode {
     private Map<String, double[]> pontosDeOnibus;
 
-    public OnibusNode(Date date, double latitude, double longitude, UUID uuid, Map<String, double[]> pontosDeOnibus){
-        super(date, latitude, longitude, uuid); 
+    public OnibusNode(UUID uuid, double latitude, double longitude, Map<String, double[]> pontosDeOnibus){
+        super(uuid, latitude, longitude);
         this.pontosDeOnibus = pontosDeOnibus;
     }
-
-    
 
 }
