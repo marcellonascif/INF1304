65a92dc732262e02681fec133671fbbeb64ef8d7
Thomas de Mello Oliva <105178736+Thom3002@users.noreply.github.com>
2024-11-10 16:16:54 +0000

    Adiciona staticlibrary

--- ProcessingNode/src/main/ProcessingNode.java
+++ ProcessingNode/src/main/ProcessingNode.java
@@ -1,5 +1,7 @@
 package main;
 
+import main.StaticLibrary;
+
 import java.nio.charset.StandardCharsets;
 import java.lang.reflect.Field;
 import java.util.Collections;
@@ -40,33 +42,6 @@ public class ProcessingNode extends ModelApplication {
         this.swap = new Swap(objectMapper);
     }
 
-    public static void setEnv(Map<String, String> newenv) throws Exception {
-        try {
-            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-            theEnvironmentField.setAccessible(true);
-            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-            env.putAll(newenv);
-            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-            theCaseInsensitiveEnvironmentField.setAccessible(true);
-            Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
-            cienv.putAll(newenv);
-        }
-        catch (NoSuchFieldException e) {
-            Class[] classes = Collections.class.getDeclaredClasses();
-            Map<String, String> env = System.getenv();
-            for (Class cl : classes) {
-                if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-                    Field field = cl.getDeclaredField("m");
-                    field.setAccessible(true);
-                    Object obj = field.get(env);
-                    Map<String, String> map = (Map<String, String>) obj;
-                    map.clear();
-                    map.putAll(newenv);
-                }
-            }
-        }
-    }
 
     /**
      * Main
@@ -86,7 +61,7 @@ public class ProcessingNode extends ModelApplication {
         if(System.getenv("app.producer.linger.ms") == null) env.put("app.producer.linger.ms", "1");
         if(System.getenv("app.producer.acks") == null) env.put("app.producer.acks", "all");
        try {
-           setEnv(env);
+           StaticLibrary.setEnv(env);
        }
        catch (Exception e) {
            e.printStackTrace();
