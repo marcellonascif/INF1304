7b58cce757b45641f79d2ca4a9ad30cdd25e722a
Thomas de Mello Oliva <105178736+Thom3002@users.noreply.github.com>
2024-11-12 20:59:23 +0000

    adiciona funcoes na StaticLibrary

--- util/src/main/java/main/PontoDeOnibus.java
+++ util/src/main/java/main/PontoDeOnibus.java
@@ -3,14 +3,12 @@ package main;
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import java.util.HashSet;
 import java.util.Set;
+import main.StaticLibrary;
 
 public class PontoDeOnibus {
     private int numeroGrupo;
     private String nomePonto;
     private Coordinate coordenada;
-
-    // Constante para o raio da Terra em metros
-    private static final double RAIO_DA_TERRA_METROS = 6371e3;
 
     public PontoDeOnibus(int numeroGrupo, String nomePonto, double latitude, double longitude) {
         this.numeroGrupo = numeroGrupo;
@@ -39,7 +37,6 @@ public class PontoDeOnibus {
                 '}';
     }
 
-
     /**
      * Método que verifica se a coordenada dada está dentro de um raio de qualquer ponto de ônibus no HashSet
      *
@@ -49,7 +46,7 @@ public class PontoDeOnibus {
      */
     public boolean estaDentroDoRaio(Coordinate coord_mn, double raio) {
 
-        double distancia = calcularDistancia(coord_mn, this.coordenada);
+        double distancia = StaticLibrary.calcularDistancia(coord_mn, this.coordenada);
         System.out.printf("Distância até o ponto de ônibus %s(%d): %.2f metros\n", this.nomePonto, this.numeroGrupo, distancia);
         if (distancia <= raio) {
             return true;
@@ -57,41 +54,4 @@ public class PontoDeOnibus {
 
         return false;
     }
-
-    /**
-     * Método que calcula a distância entre duas coordenadas usando a fórmula de Haversine
-     *
-     * @param coord1 Primeira coordenada
-     * @param coord2 Segunda coordenada
-     * @return Distância entre as duas coordenadas em metros
-     */
-    static public double calcularDistancia(Coordinate coord1, Coordinate coord2) {
-        double latitude1 = Math.toRadians(coord1.getLat());
-        double longitude1 = Math.toRadians(coord1.getLon());
-        double latitude2 = Math.toRadians(coord2.getLat());
-        double longitude2 = Math.toRadians(coord2.getLon());
-
-        double deltaLat = latitude2 - latitude1;
-        double deltaLon = longitude2 - longitude1;
-
-        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
-                + Math.cos(latitude1) * Math.cos(latitude2)
-                * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        // Distância final em metros
-        return RAIO_DA_TERRA_METROS * c;
-    }
-
-    static public double calcularTempo(Double distancia, Double velocidade) {
-        double tempo = (distancia / (velocidade / 3.6)) / 60;
-
-        if (tempo < 1) {
-            return 1;
-        }
-
-        return tempo;
-    }
-
 }
--- util/src/main/java/main/StaticLibrary.java
+++ util/src/main/java/main/StaticLibrary.java
@@ -4,7 +4,11 @@ import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.Map;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+
 public class StaticLibrary {
+    private static final double RAIO_DA_TERRA_METROS = 6371e3;
+
     public static void setEnv(Map<String, String> newenv) throws Exception {
         try {
             Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
@@ -31,5 +35,41 @@ public class StaticLibrary {
                 }
             }
         }
+    }
+
+    /**
+     * Método que calcula a distância entre duas coordenadas usando a fórmula de Haversine
+     *
+     * @param coord1 Primeira coordenada
+     * @param coord2 Segunda coordenada
+     * @return Distância entre as duas coordenadas em metros
+     */
+    static public double calcularDistancia(Coordinate coord1, Coordinate coord2) {
+        double latitude1 = Math.toRadians(coord1.getLat());
+        double longitude1 = Math.toRadians(coord1.getLon());
+        double latitude2 = Math.toRadians(coord2.getLat());
+        double longitude2 = Math.toRadians(coord2.getLon());
+
+        double deltaLat = latitude2 - latitude1;
+        double deltaLon = longitude2 - longitude1;
+
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+                + Math.cos(latitude1) * Math.cos(latitude2)
+                * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Distância final em metros
+        return RAIO_DA_TERRA_METROS * c;
+    }
+
+    static public double calcularTempo(Double distancia, Double velocidade) {
+        double tempo = (distancia / (velocidade / 3.6)) / 60;
+
+        if (tempo < 1) {
+            return 1;
+        }
+
+        return tempo;
     }
 }
